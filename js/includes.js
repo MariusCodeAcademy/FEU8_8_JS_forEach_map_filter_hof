abca9930f0af6fa8f3281653b5a4b4a9febbb3c8
Marius CA <mariuskcodeacademy@gmail.com>
2023-07-05 11:03:13 +0300

    incl

--- js/includes.js
+++ js/includes.js
@@ -9,3 +9,10 @@ console.log('numsArr ===', numsArr);
 // ar -4 yra masyve
 let arYraM4 = numsArr.includes(-4);
 console.log('arYraM4 ===', arYraM4);
+
+let arYraM4Some = numsArr.some((sk) => sk < -14);
+console.log('arYraM4Some ===', arYraM4Some);
+
+// ar visos reiksmes yra daugiau uz 0
+const allPositives = numsArr.every((sk) => sk > 0);
+console.log('allPositives ===', allPositives);
